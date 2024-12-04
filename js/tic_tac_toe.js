47dfd65fec79e18ff1c754feffd48fd29dcdddc6
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2024-12-03 22:06:04 -0600

    Create constants that state the assumptions for the initial board

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -14,4 +14,18 @@ const tic_tac_toe_game = (function(win, doc) {
 	    getScore
 	}
     }
+
+    // gameboard coordinates are layed out as a 3x3 grid with y=0 being the
+    // bottom and x=0 being the left
+    let gameboard = (function () {
+	const SPACE_STATES = {
+	    UNUSED : " ",
+	    X : "X",
+	    O : "O"
+	};
+	const BOARD_SIDE_LENGTH = 3;
+	const INITIAL_BOARD =
+	    Array.from({length: BOARD_SIDE_LENGTH},
+		() => Array(BOARD_SIDE_LENGTH).fill(SPACE_STATES.UNUSED));
+    }
 })(window, document);
