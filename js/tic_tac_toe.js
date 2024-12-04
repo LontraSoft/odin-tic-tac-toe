bde55cbca4b759ac66c2cb21b93aed3082a5dad0
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2024-12-03 21:24:50 -0600

    Create the scoreboard

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -1,2 +1,17 @@
 const tic_tac_toe_game = (function(win, doc) {
+    function Scoreboard(playerCount) {
+	scores = new Array(playerCount).fill(0);
+	
+	function incrementScore(playerNumber) {
+	    scores[playerNumber] += 1;
+	}
+	function getScore(playerNumber) {
+	    return scores[playerNumber];
+	}
+	
+	return {
+	    incrementScore,
+	    getScore
+	}
+    }
 })(window, document);
